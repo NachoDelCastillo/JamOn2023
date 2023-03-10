1df8f7a72bce2fb0b812665a080e40a22a0b9bd9
Javier <javilleg@ucm.es>
2023-03-10 23:46:15 +0100

    SPAWNER ELIMINA LOS OBJETOS QUE CREA

--- WrongAnswer_JamOn2023/Assets/Scripts/Spawner.cs
+++ WrongAnswer_JamOn2023/Assets/Scripts/Spawner.cs
@@ -1,4 +1,5 @@
 using Dreamteck.Splines;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -8,6 +9,8 @@ public class Spawner : MonoBehaviour
 {
     [SerializeField]
     GameObject toSpawn;
+    [SerializeField]
+    Transform player;
     [SerializeField]
     SplineFollower boss;
     [SerializeField]
@@ -25,6 +28,7 @@ public class Spawner : MonoBehaviour
     LayerMask trackLayer;
 
     List<GameObject> spawns = new List<GameObject>();
+    List<double> spawnperc = new List<double>();
 
     private void Start()
     {
@@ -45,7 +49,7 @@ public class Spawner : MonoBehaviour
             cont += Time.deltaTime;
         }
 
-
+        DespawnPassedObjects();
     }
 
     private double GetWhereToSpawnPercent()
@@ -70,8 +74,35 @@ public class Spawner : MonoBehaviour
 
         if (Physics.Raycast(referencia.position, rayDir, out RaycastHit hit, 300, ~trackLayer))
         {
-            spawns.Insert(0, Instantiate(toSpawn, hit.point, rotacionFinal));
+            spawns.Add(Instantiate(toSpawn, hit.point, rotacionFinal));
+            spawnperc.Add(percentInTrack);
         }
+    }
+
+    private void DespawnPassedObjects()
+    {
+        SplineSample nearestPer = new SplineSample();
+        track.Project(nearestPer, player.transform.position);
+        double minClamp = (nearestPer.percent - 0.1) < 0 ? nearestPer.percent + 0.9 : nearestPer.percent - 0.1;
+
+        double maxClamp = (minClamp + 0.5) > 1.0 ? minClamp - 0.5 : minClamp + 0.5;
+
+        Debug.Log(minClamp);
+
+        if (minClamp < 0.5)
+            if (!(spawnperc[0] > minClamp && spawnperc[0] < maxClamp))
+            {
+                spawnperc.RemoveAt(0);
+                Destroy(spawns[0]);
+                spawns.RemoveAt(0);
+            }
+        else
+            if (spawnperc[0] < minClamp || spawnperc[0] > maxClamp)
+            {
+                spawnperc.RemoveAt(0);
+                Destroy(spawns[0]);
+                spawns.RemoveAt(0);
+            }
     }
 
 }
