77ff111d977f4c289f360cbf3f9bf6c78d591649
sarais02 <sarais02@ucm.es>
2023-03-11 23:40:16 +0100

    AHORA SI

--- WrongAnswer_JamOn2023/Assets/Scripts/PowerUps/PowerUp.cs
+++ WrongAnswer_JamOn2023/Assets/Scripts/PowerUps/PowerUp.cs
@@ -1,28 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public  abstract class PowerUp : MonoBehaviour{
+public abstract class PowerUp : MonoBehaviour
+{
 
 
     [SerializeField]
     float rotacion = 0.1f;
-    Vector3 ejeRotacion = new Vector3(0,1,0);
+    Vector3 ejeRotacion = new Vector3(0, 1, 0);
     // Start is called before the first frame update
-    void Start(){        
+    void Start()
+    {
     }
-    private void Update(){
+    private void Update()
+    {
         RotatePowerUp();
     }
     public void RotatePowerUp()
     {
         transform.Rotate(ejeRotacion, rotacion);
     }
-    private void OnTriggerEnter(Collider other){
-        if(other.gameObject.GetComponent<ShipController>() != null) {
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<ShipController>() != null)
+        {
             Power(other.gameObject);
-            //this.enabled = false;
-            //this.gameObject.SetActive(false);
+            this.enabled = false;
+            this.gameObject.SetActive(false);
         }
     }
     public virtual void Power(GameObject player) { }
